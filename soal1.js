f47204777e65cbaeeede5d08e0e076aa3e1b9300
Akhdan Prajadisa <akhdanprajadisa962@gmail.com>
2025-10-02 23:18:45 +0700

    Paham dengan System sederhana

--- soal1.js
+++ soal1.js
@@ -1,33 +1,13 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 
-//soal 1
-while (halo != 10){
-let halo = Number(prompt("Jam Masuk: "))}
-
-
-let tost = Number(prompt("Jam keluar: "));
-
-
-if (tost = 11){
-    console.log("Lama Bekerja 1 jam");
-} else if (tost = 2){
-    console.log("Lama bekerja 4 jama");
-} else if (tost = 7){
-    console.log("Lama berkerja 9 jam");
+let jamMasuk = Number(prompt("Masukan Jam masuk Anda :"))
+let jamKeluar = Number(prompt ("Masukan Jam Keluar :"))
+let selisih
+if (jamMasuk <= jamKeluar){
+    selisih = jamKeluar - jamMasuk
+}else if (jamMasuk >= jamKeluar) {
+    selisih = (12 - jamMasuk) + jamKeluar
 }
 
-
-//soal 2
-let Tarif1 = 2000
-let Tarif2 = 500
-let biaya = 0
-let Parkir = Number(prompt("Jam Masuk: "))
-let keluar = Number(prompt("Jam Keluar: "))
-let lama =0
-if (parkir == 10 && keluar == 11){
-    lama = 1
-    
-}
-
-
+console.log(selisih)
